3db9157c5feda44bd22bc3e6cca33ec2747c8ae1
Osujdenie <osujdenie@yandex.ru>
2023-08-02 04:20:04 +0300

    1st push

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public abstract class State
+{
+    protected Character character;
+    protected StateMachine stateMachine;
+
+    protected State(Character character, StateMachine stateMachine)
+    {
+        this.character = character;
+        this.stateMachine = stateMachine;
+    }
+
+    public virtual void Enter()
+    {
+        //DisplayOnUI(UIManager.Alignment.Left);
+    }
+
+    public virtual void HandleInput()
+    {
+
+    }
+
+    public virtual void LogicUpdate()
+    {
+
+    }
+
+    public virtual void PhysicsUpdate()
+    {
+
+    }
+
+    public virtual void Exit()
+    {
+
+    }
+
+    /*
+    protected void DisplayOnUI(UIManager.Alignment alignment)
+    {
+        UIManager.Instance.Display(this, alignment);
+    }
+    */
+}
